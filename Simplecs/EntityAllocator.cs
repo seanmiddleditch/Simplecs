01558b775f09dd56adbeb9345a71fa51d1986ce3
Sean Middleditch <sean.middleditch@gmail.com>
2019-09-10 23:16:46 -0700

    Smarter entity key allocation

--- Simplecs/EntityAllocator.cs
+++ Simplecs/EntityAllocator.cs
@@ -11,29 +11,106 @@
 // <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Simplecs {
     /// <summary>
     /// Allocates and maintains a unique set of entity keys.
     /// </summary>
     internal class EntityAllocator {
-        private uint _nextId = 1;
+        private uint _nextIndex = 1;
         private List<uint> _freeList = new List<uint>();
+        private List<byte> _generations = new List<byte>();
+        private int _freeHead = 0;
+        private int _freeCount = 0;
+        private uint _freeMinimum = 1000;
 
         public const uint invalid = 0;
 
-        public uint Allocate() {
-            if (_freeList.Count != 0) {
-                uint id = _freeList[_freeList.Count - 1];
-                _freeList.RemoveAt(_freeList.Count - 1);
-                return id;
-            }
+        public EntityAllocator() { }
 
-            return _nextId++;
+        public EntityAllocator(uint freeMinimum) {
+            _freeMinimum = freeMinimum;
         }
 
-        public void Deallocate(uint id) {
-            _freeList.Add(id);
+        public uint Allocate() {
+            uint index = AllocateIndex();
+
+            // Determine the generation of the key at the given index;
+            // for new indices, create a new generation.
+            //
+            if (index >= _generations.Count) {
+                _generations.AddRange(Enumerable.Repeat((byte)0, (int)index - _generations.Count + 1));
+            }
+            byte generation = _generations[(int)index];
+
+            return MakeKey(index, generation);
+        }
+
+        public void Deallocate(uint key) {
+            (uint index, byte generation) = DecomposeKey(key);
+            if (index != 0 && index < _generations.Count && _generations[(int)index] == generation) {
+                ++_generations[(int)index];
+                AddToFreeList(index);
+            }
+        }
+
+        public bool Validate(uint key) {
+            (uint index, byte generation) = DecomposeKey(key);
+            return index != 0 && key < _generations.Count &&  _generations[(int)index] == generation;
+        }
+
+        private void AddToFreeList(uint key) {
+            // If the freelist is full, grow.
+            //
+            if (_freeCount == _freeList.Count) {
+                _freeList.Add(key);
+                ++_freeCount;
+                return;
+            }
+
+            // Insert into the circular buffer.
+            //
+            int index = _freeHead + _freeCount;
+            if (index > _freeList.Count) {
+                index -= _freeList.Count;
+            }
+
+            _freeList[index] = key;
+            ++_freeCount;
+        }
+
+        private uint AllocateIndex() {
+            // Only consume from the freelist if we have some items in it,
+            // to avoid recycling the same id too often.
+            //
+            if (_freeCount <= _freeMinimum) {
+                return _nextIndex++;
+            }
+
+            uint index = _freeList[_freeHead];
+
+            // Update the circular buffer after consuming the head item.
+            //
+            --_freeCount;
+            if (++_freeHead >= _freeList.Count) {
+                _freeHead = 0;
+            }
+
+            return index;
+        }
+
+        [MethodImplAttribute(MethodImplOptions.AggressiveInlining)]
+        public static uint MakeKey(uint index, byte generation) {
+            return ((uint)generation << 24) | (index & 0x00FFFFFF);
+        }
+
+        [MethodImplAttribute(MethodImplOptions.AggressiveInlining)]
+        public static (uint index, byte generation) DecomposeKey(uint key) {
+            uint index = key & 0x00FFFFFF;
+            byte generation = (byte)(key >> 24);
+            return (index, generation);
         }
     }
 }
--- SimplecsTests/EntityAllocatorTest.cs
+++ SimplecsTests/EntityAllocatorTest.cs
@@ -5,7 +5,7 @@ namespace SimplecsTests {
     public class EntityAllocatorTest {
         [Test]
         public void AllocateAndFreeList() {
-            var allocator = new EntityAllocator();
+            var allocator = new EntityAllocator(freeMinimum:0);
 
             Assert.AreEqual(expected:1u, actual:allocator.Allocate());
             Assert.AreEqual(expected:2u, actual:allocator.Allocate());
@@ -15,9 +15,14 @@ namespace SimplecsTests {
             allocator.Deallocate(3u);
             allocator.Deallocate(1u);
 
-            Assert.AreEqual(expected:1u, actual:allocator.Allocate());
-            Assert.AreEqual(expected:3u, actual:allocator.Allocate());
-            Assert.AreEqual(expected:2u, actual:allocator.Allocate());
+            // Generation will be bumped for the recycled indices.
+            //
+            Assert.AreEqual(expected:EntityAllocator.MakeKey(2u, 1), actual:allocator.Allocate());
+            Assert.AreEqual(expected:EntityAllocator.MakeKey(3u, 1), actual:allocator.Allocate());
+            Assert.AreEqual(expected:EntityAllocator.MakeKey(1u, 1), actual:allocator.Allocate());
+
+            // But not for new indices.
+            //
             Assert.AreEqual(expected:4u, actual:allocator.Allocate());
         }
     }
