f2036b6a22117096595d49cda1f880b542a09a9c
Sean Middleditch <sean.middleditch@gmail.com>
2019-09-11 14:54:15 -0700

    Move test components to their own file
    Also add Destroy tests

--- SimplecsTests/ViewTest.cs
+++ SimplecsTests/ViewTest.cs
@@ -7,12 +7,12 @@ namespace SimplecsTests {
         [Test]
         public void EachMutable() {
             var world = new World();
-            world.Create().Attach(new Comp2 { x = 1 });
-            world.Create().Attach(new Comp2 { x = 2 });
-            world.Create().Attach(new Comp2 { x = 3 });
+            world.Create().Attach(new IntComponent { x = 1 });
+            world.Create().Attach(new IntComponent { x = 2 });
+            world.Create().Attach(new IntComponent { x = 3 });
 
-            var view = new View<Comp2>(world);
-            view.Each((Entity _, ref Comp2 comp) => comp.x *= comp.x);
+            var view = new View<IntComponent>(world);
+            view.Each((Entity _, ref IntComponent comp) => comp.x *= comp.x);
 
             Assert.AreEqual(expected: 1, actual: view.ElementAt(0).Item2.x);
             Assert.AreEqual(expected: 4, actual: view.ElementAt(1).Item2.x);
@@ -22,12 +22,12 @@ namespace SimplecsTests {
         [Test]
         public void Require() {
             var world = new World();
-            world.Create().Attach(new Comp2 { x = 1 });
-            world.Create().Attach(new Comp1 { name = "Bob" }).Attach(new Comp2 { x = 2 });
-            world.Create().Attach(new Comp2 { x = 3 });
-            world.Create().Attach(new Comp1 { name = "Susan" }).Attach(new Comp2 { x = 4 });
+            world.Create().Attach(new IntComponent { x = 1 });
+            world.Create().Attach(new NameComponent { name = "Bob" }).Attach(new IntComponent { x = 2 });
+            world.Create().Attach(new IntComponent { x = 3 });
+            world.Create().Attach(new NameComponent { name = "Susan" }).Attach(new IntComponent { x = 4 });
 
-            var view = new View<Comp2>(world).Require<Comp1>();
+            var view = new View<IntComponent>(world).Require<NameComponent>();
 
             Assert.AreEqual(expected: 2, actual: view.ElementAt(0).Item2.x);
             Assert.AreEqual(expected: 4, actual: view.ElementAt(1).Item2.x);
@@ -36,15 +36,40 @@ namespace SimplecsTests {
         [Test]
         public void Exclude() {
             var world = new World();
-            world.Create().Attach(new Comp1 { name = "Bob" }).Attach(new Comp2 { x = 2 });
-            world.Create().Attach(new Comp2 { x = 1 });
-            world.Create().Attach(new Comp1 { name = "Susan" }).Attach(new Comp2 { x = 4 });
-            world.Create().Attach(new Comp2 { x = 3 });
+            world.Create().Attach(new NameComponent { name = "Bob" }).Attach(new IntComponent { x = 2 });
+            world.Create().Attach(new IntComponent { x = 1 });
+            world.Create().Attach(new NameComponent { name = "Susan" }).Attach(new IntComponent { x = 4 });
+            world.Create().Attach(new IntComponent { x = 3 });
 
-            var view = new View<Comp2>(world).Exclude<Comp1>();
+            var view = new View<IntComponent>(world).Exclude<NameComponent>();
 
             Assert.AreEqual(expected: 1, actual: view.ElementAt(0).Item2.x);
             Assert.AreEqual(expected: 3, actual: view.ElementAt(1).Item2.x);
+        }
+
+        [Test]
+        public void Match() {
+            var world = new World();
+            var entity = world.Create()
+                .Attach(new NameComponent { name = "Bob" })
+                .Attach(new IntComponent { x = 7 })
+                .Entity;
+
+            world.Create()
+                .Attach(new NameComponent { name = "Susan" })
+                .Attach(new IntComponent { x = 90 });
+
+            var nameView = new View<NameComponent>(world);
+            var intView = new View<IntComponent>(world);
+            var bothView = new View<NameComponent, IntComponent>(world);
+
+            Assert.IsTrue(nameView.Any());
+            Assert.IsTrue(intView.Any());
+            Assert.IsTrue(bothView.Any());
+
+            Assert.AreEqual(expected: (entity, new NameComponent { name = "Bob" }), actual: nameView.FirstOrDefault());
+            Assert.AreEqual(expected: (entity, new IntComponent { x = 7 }), actual: intView.FirstOrDefault());
+            Assert.AreEqual(expected: (entity, new NameComponent { name = "Bob" }, new IntComponent { x = 7 }), actual: bothView.FirstOrDefault());
         }
     }
 }
