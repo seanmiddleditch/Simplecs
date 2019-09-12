affb8698076229650fd526ae456e4f1659b33099
Sean Middleditch <sean.middleditch@gmail.com>
2019-09-11 17:48:38 -0700

    Remove ViewBase to simplify builder

--- Simplecs/ViewBuilder.cs
+++ Simplecs/ViewBuilder.cs
@@ -22,8 +22,7 @@ namespace Simplecs {
     /// </summary>
     public class ViewBuilder {
         private World _world;
-        private List<IComponentTable>? _required;
-        private List<IComponentTable>? _excluded;
+        private ViewPredicate? _predicate;
 
         internal ViewBuilder(World world) => _world = world;
 
@@ -32,10 +31,8 @@ namespace Simplecs {
         /// though it will not be in the selected component set.
         /// </summary>
         public ViewBuilder Require<T>() where T : struct {
-            if (_required == null) {
-                _required = new List<IComponentTable>();
-            }
-            _required.Add(_world.GetTable<T>());
+            _predicate ??= new ViewPredicate();
+            _predicate.Require(_world.GetTable<T>());
             return this;
         }
 
@@ -44,38 +41,32 @@ namespace Simplecs {
         /// meaning that it cannot be present on matched entities.
         /// </summary>
         public ViewBuilder Exclude<T>() where T : struct {
-            if (_excluded == null) {
-                _excluded = new List<IComponentTable>();
-            }
-            _excluded.Add(_world.GetTable<T>());
+            _predicate ??= new ViewPredicate();
+            _predicate.Exclude(_world.GetTable<T>());
             return this;
         }
 
         /// <summary>
         /// Creates a View that selects the specified component.
         /// </summary>
-        public View<T> Select<T>() where T : struct {
-            var view = new View<T>(table: _world.GetTable<T>(), required: _required, excluded: _excluded);
-            _required = _excluded = null;
-            return view;
-        }
+        public View<T> Select<T>() where T : struct => new View<T>(Table<T>(), Predicate());
 
         /// <summary>
         /// Creates a View that selects the specified components.
         /// </summary>
-        public View<T1, T2> Select<T1, T2>() where T1 : struct where T2 : struct {
-            var view = new View<T1, T2>(table1: _world.GetTable<T1>(), table2: _world.GetTable<T2>(), required: _required, excluded: _excluded);
-            _required = _excluded = null;
-            return view;
-        }
+        public View<T1, T2> Select<T1, T2>() where T1 : struct where T2 : struct => new View<T1, T2>(Table<T1>(), Table<T2>(), Predicate());
 
         /// <summary>
         /// Creates a View that selects the specified components.
         /// </summary>
-        public View<T1, T2, T3> Select<T1, T2, T3>() where T1 : struct where T2 : struct where T3 : struct {
-            var view = new View<T1, T2, T3>(table1: _world.GetTable<T1>(), table2: _world.GetTable<T2>(), table3: _world.GetTable<T3>(), required: _required, excluded: _excluded);
-            _required = _excluded = null;
-            return view;
+        public View<T1, T2, T3> Select<T1, T2, T3>() where T1 : struct where T2 : struct where T3 : struct => new View<T1, T2, T3>(Table<T1>(), Table<T2>(), Table<T3>(), Predicate());
+
+        private ViewPredicate Predicate() {
+            var predicate = _predicate ?? new ViewPredicate();
+            _predicate = null;
+            return predicate;
         }
+
+        private ComponentTable<T> Table<T>() where T : struct => _world.GetTable<T>();
     }
 }
